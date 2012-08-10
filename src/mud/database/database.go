c25848a8f72e0a420c8e09b2150c9c8f88872a81
Chris Thatcher <thatchman@gmail.com>
2012-08-09 19:49:52 -0600

    Added NewUser function and a dbError type

--- src/mud/database/database.go
+++ src/mud/database/database.go
@@ -1,19 +1,46 @@
-package db
+package database
 
 import (
-    "labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
-func FindUser( session *mgo.Session, name string ) bool {
-    c := session.DB("mud").C("users")
-    q := c.Find(bson.M{"name": name})
-
-    count, err := q.Count()
-
-    if err != nil {
-        return false
-    }
-
-    return count > 0
+type dbError struct {
+    message string
 }
+
+func (self dbError) Error() string {
+    return self.message
+}
+
+func newDbError( message string ) dbError {
+    var err dbError
+    err.message = message
+    return err
+}
+
+func FindUser(session *mgo.Session, name string) bool {
+	c := session.DB("mud").C("users")
+	q := c.Find(bson.M{"name": name})
+
+	count, err := q.Count()
+
+	if err != nil {
+		return false
+	}
+
+	return count > 0
+}
+
+func NewUser(session *mgo.Session, name string) error {
+	if FindUser(session, name) {
+		return newDbError("That user already exists")
+	}
+
+	c := session.DB("mud").C("users")
+	c.Insert(bson.M{"name": name})
+
+	return nil
+}
+
+// vim: nocindent
