8f18df40872b65e6a02d428b1b8a41fc9c352073
Chris Thatcher <chris.thatcher@raytheon.com>
2012-08-13 18:02:43 -0600

    Support for the "l" command, a little cleanup of user input processing

--- src/mud/game/game.go
+++ src/mud/game/game.go
@@ -14,35 +14,44 @@ func processCommand(session *mgo.Session, conn net.Conn, command string ) {
     fmt.Printf("Processing command: %v\n", command)
 
     switch command {
-        case "new":
+        case "?": fallthrough
+        case "help":
+        case "dig":
         case "edit":
             // Enter edit mode (show room with [] markers indicating portions that can be modified)
+        default:
+            io.WriteString(conn, "Unrecognized command")
     }
 }
 
 func Exec(session *mgo.Session, conn net.Conn, character string) {
 	utils.WriteLine(conn, "Welcome, "+utils.FormatName(character))
+
+    room, err := database.GetCharacterRoom(session, character)
+    utils.WriteLine(conn, room.ToString())
+
 	for {
-		room, err := database.GetCharacterRoom(session, character)
         utils.PanicIfError(err)
 
-		utils.WriteLine(conn, room.ToString())
 		input := utils.GetUserInput(conn, "\n> ")
 
         if strings.HasPrefix(input, "/") {
             processCommand( session, conn, input[1:len(input)] )
-        }
-
-		if input == "quit" || input == "exit" {
-			utils.WriteLine(conn, "Goodbye")
-			conn.Close()
-			break
-		}
-
-        if room.HasExit(input) {
-            database.SetCharacterRoom(session, character, room.ExitId(input))
         } else {
-            io.WriteString(conn, "You can't do that.")
+            switch input {
+                case "quit": fallthrough
+                case "exit":
+                    utils.WriteLine(conn, "Goodbye")
+                    conn.Close()
+                case "l":
+                    utils.WriteLine(conn, room.ToString())
+                default:
+                    if room.HasExit(input) {
+                        database.SetCharacterRoom(session, character, room.ExitId(input))
+                    } else {
+                        io.WriteString(conn, "You can't do that")
+                    }
+            }
         }
 	}
 }
