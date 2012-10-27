20f37dfb704faed012b1c9969ad7dde3546334a8
Chris Thatcher <chris.thatcher@raytheon.com>
2012-10-27 10:56:46 -0600

    Fix the exit menu

--- src/mud/game/game.go
+++ src/mud/game/game.go
@@ -31,13 +31,13 @@ func getToggleExitMenu(room database.Room) utils.Menu {
 	menu := utils.NewMenu("Edit Exits")
 
 	menu.AddAction("n", "[N]orth: "+onOrOff(database.DirectionNorth))
-	menu.AddAction("ne", "[NE]North East"+onOrOff(database.DirectionNorthEast))
+	menu.AddAction("ne", "[NE]North East: "+onOrOff(database.DirectionNorthEast))
 	menu.AddAction("e", "[E]ast: "+onOrOff(database.DirectionEast))
-	menu.AddAction("se", "[SE]South East"+onOrOff(database.DirectionSouthEast))
+	menu.AddAction("se", "[SE]South East: "+onOrOff(database.DirectionSouthEast))
 	menu.AddAction("s", "[S]outh: "+onOrOff(database.DirectionSouth))
-	menu.AddAction("sw", "[SW]South West"+onOrOff(database.DirectionSouthWest))
+	menu.AddAction("sw", "[SW]South West: "+onOrOff(database.DirectionSouthWest))
 	menu.AddAction("w", "[W]est: "+onOrOff(database.DirectionWest))
-	menu.AddAction("nw", "[NW]North West"+onOrOff(database.DirectionNorthWest))
+	menu.AddAction("nw", "[NW]North West: "+onOrOff(database.DirectionNorthWest))
 	menu.AddAction("u", "[U]p: "+onOrOff(database.DirectionUp))
 	menu.AddAction("d", "[D]own: "+onOrOff(database.DirectionDown))
 
