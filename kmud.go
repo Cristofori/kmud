902a656d5ee4bfacfa3ece93cfb6504f72c9acd3
Chris Thatcher <cthatcher@hirevue.com>
2015-06-09 15:31:59 -0600

    Added a signal handler that prints the stack trace when we interrupt the server

--- kmud.go
+++ kmud.go
@@ -1,11 +1,30 @@
 package main
 
-import "kmud/server"
-import "runtime"
+import (
+	"kmud/server"
+	"os"
+	"os/signal"
+	"runtime"
+)
 
 func main() {
 	runtime.GOMAXPROCS(8)
 
+	go signalHandler()
+
 	var s server.Server
 	s.Exec()
+}
+
+func signalHandler() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	for {
+		<-c
+		stack := make([]byte, 1024*10)
+		runtime.Stack(stack, true)
+		os.Stderr.Write(stack)
+		os.Exit(0)
+	}
 }
